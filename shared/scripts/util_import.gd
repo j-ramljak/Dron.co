bf159b63b5f7de6e44f4419b607c91f1142c0985
Martin Golub <martin.golub@fer.unizg.hr>
2025-12-07 12:34:15 +0100

    Tree generator

--- shared/scripts/util_import.gd
+++ shared/scripts/util_import.gd
@@ -1,8 +1,11 @@
 @tool
 extends EditorScenePostImport
 
-const MATERIAL = preload("res://shared/materials/palette_color.tres")
-
+const MATERIALS := {
+	"palette_color" = preload("res://shared/materials/palette_color.tres"),
+	"palette_foliage" = preload("res://shared/materials/palette_foliage.tres")
+}
+	
 func _post_import(scene):
 	iterate(scene)
 	if scene.get_child_count(true) == 1:
@@ -19,9 +22,17 @@ func iterate(node: Node):
 		for child in node.get_children():
 			iterate(child)
 			if child is MeshInstance3D:
-				child.mesh.surface_set_material(0, MATERIAL)
+				set_material(child)
 
 func set_children_owner(node, owner):
 	for n in node.get_children():
 		n.owner = owner
 		set_children_owner(n, owner)
+
+func set_material(node: MeshInstance3D):
+	for index in range(node.mesh.get_surface_count()):
+		for key in MATERIALS.keys():
+			var name = node.mesh.surface_get_material(index).resource_name
+			if key == name and MATERIALS.has(name):
+				node.mesh.surface_set_material(index, MATERIALS[key])
+			
