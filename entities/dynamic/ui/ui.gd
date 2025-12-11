28c434fc54badbe52ee27c9cc1f12063d059f714
Martin Golub <martin.golub@fer.unizg.hr>
2025-12-11 11:06:06 +0100

    UI layout, theme

--- entities/dynamic/ui/ui.gd
+++ entities/dynamic/ui/ui.gd
@@ -1,6 +1,24 @@
 extends Control
 
+@export var HUD: Control
+@export var MAIN_MENU: Control
+@export var GAME_OVER: Control
 @export var CHARGE_DISPLAY: Label
+
+func _ready() -> void:
+	MAIN_MENU.visible = true
+	HUD.visible = false
+	GAME_OVER.visible = false
 
 func update_charge(charge: float):
 	CHARGE_DISPLAY.text = str(charge)
+
+func close_main_menu():
+	MAIN_MENU.visible = false
+	HUD.visible = true
+
+func close_game_over():
+	GAME_OVER.visible = false
+
+func quit():
+	get_tree().quit()
