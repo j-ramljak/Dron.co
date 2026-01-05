a73cabd86b50855ca9d4d1fa1d781e6bf17117a8
j-ramljak <dagi.ramljak@gmail.com>
2026-01-05 01:27:54 +0100

    Added global timer and fixed collison for charging

--- entities/dynamic/user_interface/user_interface.gd
+++ entities/dynamic/user_interface/user_interface.gd
@@ -19,8 +19,16 @@ enum Menu { MAIN_MENU, HUD, GAME_OVER }
 @onready var delivery_label = $HUD/TopBar/MarginContainer/HBoxContainer/DeliveryDisplay/MarginContainer/BoxContainer/DeliveryValue
 var delivered_pacakges: int = 0
 
+@onready var global_timer = $"HUD/TopBar/MarginContainer/HBoxContainer/TimeDisplay/Global timer" 
+@onready var global_timer_label = $HUD/TopBar/MarginContainer/HBoxContainer/TimeDisplay/MarginContainer/BoxContainer/TimeValue
+
 func _ready() -> void:
 	goto_menu(START_AT)
+	
+
+	
+func _process(_delta: float) -> void:
+	global_timer_label.text  = "%02d:%02d" % time_left_global()
 
 func quit():
 	get_tree().quit()
@@ -58,6 +66,18 @@ func demo_died():
 func update_delivery_count(value: int):
 	#potrebno u editoru ih connectat
 	delivered_pacakges += value
-	print(delivered_pacakges)
 	delivery_label.text = str(delivered_pacakges)
 	
+func time_left_global():
+	var time_left = global_timer.time_left
+	var minute = floor(time_left / 60)
+	var second = int(time_left) % 60
+	return [minute, second]
+
+func _on_global_timer_timeout() -> void:
+	demo_die()
+
+
+func _on_start_button_pressed() -> void:
+	goto_menu(Menu.HUD)
+	global_timer.start()
