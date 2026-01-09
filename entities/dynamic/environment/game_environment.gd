eb4ba69850df4ba1951d95b8bef3e260030b608c
Martin Golub <martin.golub@fer.unizg.hr>
2026-01-09 19:10:39 +0100

    Water environment

--- entities/dynamic/environment/game_environment.gd
+++ entities/dynamic/environment/game_environment.gd
@@ -5,13 +5,16 @@ class_name GameEnvironment
 @onready var SUN: DirectionalLight3D = $DirectionalLight3D
 @onready var ENVIRONMENT: WorldEnvironment = $WorldEnvironment
 @onready var OUTLINE: MeshInstance3D = $OutlineEffect
+@onready var WATER: MeshInstance3D = $Water
 
 func set_graphics_low():
 	OUTLINE.visible = false
 	ENVIRONMENT.environment.ssao_enabled = false
 	SUN.directional_shadow_mode = DirectionalLight3D.ShadowMode.SHADOW_ORTHOGONAL
+	WATER.mesh.material.set_shader_parameter("ssr_enabled", false)
 	
 func set_graphics_high():
 	OUTLINE.visible = true
 	ENVIRONMENT.environment.ssao_enabled = true
 	SUN.directional_shadow_mode = DirectionalLight3D.ShadowMode.SHADOW_PARALLEL_4_SPLITS
+	WATER.mesh.material.set_shader_parameter("ssr_enabled", true)
