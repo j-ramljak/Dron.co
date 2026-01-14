ed343db3971a9dfb158d979c7f76e3affa82c52d
Martin Golub <martin.golub@fer.unizg.hr>
2026-01-14 11:54:47 +0100

    Delivery point remodel

--- entities/dynamic/delivery_point/delivery_point.gd
+++ entities/dynamic/delivery_point/delivery_point.gd
@@ -1,7 +1,16 @@
+@icon("./delivery_point.png")
 extends Area3D
+class_name DeliveryPoint3D
 
 signal package_delivered(value:int)
 @onready var confetti = preload("res://entities/dynamic/delivery_point/confetti.tscn")
+@onready var RING := $delivery_point_model/delivery_point_ring
+
+func _ready() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD).set_loops()
+	tween.tween_property(RING, "scale", Vector3(1.2, 1.2, 1.2), 0.5)
+	tween.chain().tween_property(RING, "scale", Vector3(1.0, 1.0, 1.0), 0.5)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Package3D and not body.is_being_carried:
