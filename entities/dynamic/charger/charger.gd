0ea8ae7df357da8c1cf5fdc9be7d3190c83f72b7
j-ramljak <dagi.ramljak@gmail.com>
2026-01-25 12:36:37 +0100

    Added charge particle

--- entities/dynamic/charger/charger.gd
+++ entities/dynamic/charger/charger.gd
@@ -13,6 +13,7 @@ func _on_recharge_increment_timer_timeout() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is DroneBody3D:
 		drone = body
-
+		$GPUParticles3D.emitting = true
 func _on_area_3d_body_exited(_body: Node3D) -> void:
 	drone = null
+	$GPUParticles3D.emitting = false
